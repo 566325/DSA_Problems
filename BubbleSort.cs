c3d98963632557128d160fdba1483f75551ea789
566325 <nethisireesha288@gmail.com>
2023-09-30 19:08:30 +0530

    [Sireesha]:Added Bubble sort

---
+++ BubbleSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAPrablems
+{
+    public class BubbleSort
+    {
+        public static int[] Sort(int[] array)
+        {
+            int length = array.Length;
+            for (int firstItr = 0; firstItr < length; firstItr++)
+            {
+                //flag to check if any swapping is done.
+                int flag = 0;
+                for (int secondItr = 0; secondItr < length - 1 - firstItr; secondItr++)
+                {
+                    if (array[secondItr] > array[secondItr + 1])
+                    {
+                        int temp = array[secondItr];
+                        array[secondItr] = array[secondItr + 1];
+                        array[secondItr + 1] = temp;
+                        flag++;
+                    }
+                }
+                //if no swap is done then array is sorted.
+                if (flag == 0)
+                {
+                    break;
+                }
+            }
+            return array;
+        }
+        
+        public static void Display(int[] array)
+        {
+            foreach (int item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,10 +4,13 @@
     {
         static void Main(string[] args)
         {
-            //StringPermutations obj = new StringPermutations();
-            //obj.Permutations();
-            Anagrams anagram = new Anagrams();
-            anagram.CheckAnagrams();
+            int[] array = { 20, 13, 3, 17, 2 };
+            Console.WriteLine("Unsorted array");
+            BubbleSort.Display(array);
+            Console.WriteLine("Sorted array");
+            BubbleSort.Sort(array);
+            BubbleSort.Display(array);
+
         }
     }
 }
