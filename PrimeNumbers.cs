51436411f4926c1a910d83125ae9461592d51c8d
566325 <nethisireesha288@gmail.com>
2023-09-30 19:53:20 +0530

    [Sireesha]:Added Range Of prime Numbers

---
+++ PrimeNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAPrablems
+{
+    internal class PrimeNumbers
+    {
+        internal static void CheckRangePrime()
+        {
+            long range = 1000;
+            Console.WriteLine("Prime Numbers");
+            if (range <= 1)
+                Console.WriteLine("Invalid Number");
+            else
+            {
+                for (int i = 1; i <= range; i++)
+                {
+                    int counter = 0;
+                    for (int j = i; j >= 1; j--)
+                    {
+                        if (i % j == 0)
+                        {
+                            counter++;
+                        }
+                    }
+                    if (counter == 2)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,8 +4,7 @@
     {
         static void Main(string[] args)
         {
-            BinarySearchWord.BinarySearch();
-            
+            PrimeNumbers.CheckRangePrime();
 
         }
     }
