34ba76a0ed7e802a261c64e31997f8c7c44076c5
566325 <nethisireesha288@gmail.com>
2023-09-30 19:39:09 +0530

    [Sireesha]:Added Binary Search Word

--- Program.cs
+++ Program.cs
@@ -4,12 +4,8 @@
     {
         static void Main(string[] args)
         {
-            int[] array = { 20, 13, 3, 17, 2 };
-            Console.WriteLine("Unsorted array");
-            InsertionSort.Display(array);
-            Console.WriteLine("Sorted array");
-            InsertionSort.InsertSort(array);
-            InsertionSort.Display(array);
+            BinarySearchWord.BinarySearch();
+            
 
         }
     }
